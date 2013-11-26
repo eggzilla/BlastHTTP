0d59bd69bb6e1a8c68e09513bf6bc30144f5187a
Florian Eggenhofer <egg@tbi.univie.ac.at>
2013-11-26 10:32:30 +0100

    Integrated Hlint suggestions

--- src/Bio/BlastHTTP.hs
+++ src/Bio/BlastHTTP.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE Arrows #-}
 
 -- | Searches a provided sequence with the NCBI Blast REST service and returns a blast result in xml format as String.
 --
@@ -44,7 +43,7 @@ data BlastHTTPQuery = BlastHTTPQuery
 
 -- | Parse HTML results into Xml Tree datastructure
 parseHTML :: String -> IOStateArrow s0 b0 XmlTree
-parseHTML html = readString [withParseHTML yes, withWarnings no] html         
+parseHTML = readString [withParseHTML yes, withWarnings no] 
 -- | Gets all subtrees with the specified id attribute
 atName :: ArrowXml a => String -> a XmlTree XmlTree
 atName elementId = deep (isElem >>> hasAttrValue "name" (== elementId))
@@ -58,14 +57,13 @@ startSession :: String -> String -> String -> Maybe String -> IO String
 startSession program database querySequence entrezQuery = do
   requestXml <- withSocketsDo
       $ sendEntrezQuery program database querySequence entrezQuery
-  let requestXMLString = (L8.unpack requestXml)
-  rid <- CM.liftM head (runX $ parseHTML requestXMLString //> atId "rid" >>> getAttrValue "value")
-  return rid
-
+  let requestXMLString = L8.unpack requestXml
+  CM.liftM head (runX $ parseHTML requestXMLString //> atId "rid" >>> getAttrValue "value")
+  
 -- | Send query with or without Entrez query and return response HTML
 sendEntrezQuery :: String -> String -> String -> Maybe String -> IO L8.ByteString
 sendEntrezQuery program database querySequence entrezQuery 
-  | isJust entrezQuery = simpleHttp ("http://www.ncbi.nlm.nih.gov/blast/Blast.cgi?CMD=Put&PROGRAM=" ++ program ++ "&DATABASE=" ++ database ++ "&QUERY=" ++ querySequence ++ "&ENTREZ_QUERY=" ++ (fromJust entrezQuery))
+  | isJust entrezQuery = simpleHttp ("http://www.ncbi.nlm.nih.gov/blast/Blast.cgi?CMD=Put&PROGRAM=" ++ program ++ "&DATABASE=" ++ database ++ "&QUERY=" ++ querySequence ++ "&ENTREZ_QUERY=" ++ fromJust entrezQuery)
   | otherwise = simpleHttp ("http://www.ncbi.nlm.nih.gov/blast/Blast.cgi?CMD=Put&PROGRAM=" ++ program ++ "&DATABASE=" ++ database ++ "&QUERY=" ++ querySequence)
          
 -- | Retrieve session status with RID
@@ -73,15 +71,15 @@ retrieveSessionStatus :: String -> IO String
 retrieveSessionStatus rid = do
   statusXml <- withSocketsDo
     $ simpleHttp ("http://www.ncbi.nlm.nih.gov/blast/Blast.cgi?CMD=Get&FORMAT_OBJECT=SearchInfo&RID=" ++ rid)
-  let statusXMLString = (L8.unpack statusXml)
+  let statusXMLString = L8.unpack statusXml
   return statusXMLString
-
+  
 -- | Retrieve result in blastxml format with RID 
 retrieveResult :: String -> IO (Either String BlastResult)
 retrieveResult rid = do
   statusXml <- withSocketsDo
     $ simpleHttp ("http://www.ncbi.nlm.nih.gov/blast/Blast.cgi?RESULTS_FILE=on&RID=" ++ rid ++ "&FORMAT_TYPE=XML&FORMAT_OBJECT=Alignment&CMD=Get")
-  resultXML <- (readXMLString statusXml)
+  resultXML <- readXMLString statusXml
   return (Right resultXML)
 
 -- | Check if job results are ready and then retrieves results
@@ -91,16 +89,15 @@ checkSessionStatus rid counter = do
     let counter2string = show counter2
     threadDelay 60000000
     status <- retrieveSessionStatus rid
-    results <- waitOrRetrieve status rid counter2
-    return results
+    waitOrRetrieve status rid counter2
 
 waitOrRetrieve :: String -> String -> Int -> IO (Either String BlastResult)
 waitOrRetrieve status rid counter
-  | (isInfixOf "Status=READY" status) = retrieveResult rid
-  | (isInfixOf "Status=FAILURE" status) = do
+  | "Status=READY" `isInfixOf` status = retrieveResult rid
+  | "Status=FAILURE" `isInfixOf` status = do
       let exceptionMessage = "Search $rid failed; please report to blast-help at ncbi.nlm.nih.gov.\n"
       return (Left exceptionMessage)
-  | (isInfixOf "Status=UNKNOWN" status) = do
+  | "Status=UNKNOWN" `isInfixOf` status = do
       let exceptionMessage = "Search $rid expired.\n"
       return (Left exceptionMessage)
   | otherwise = checkSessionStatus rid counter
@@ -111,8 +108,7 @@ performQuery :: String -> String -> Maybe SeqData -> Maybe String -> Int -> IO (
 performQuery program database querySequenceMaybe entrezQueryMaybe counter
   | isJust querySequenceMaybe = do 
      rid <- startSession program database (L8.unpack (unSD (fromJust querySequenceMaybe))) entrezQueryMaybe
-     result <- checkSessionStatus rid counter
-     return result
+     checkSessionStatus rid counter
   | otherwise = do 
      let exceptionMessage = "Error - no query sequence provided"
      return (Left exceptionMessage)
@@ -127,7 +123,7 @@ blastHTTP (BlastHTTPQuery program database querySequence entrezQuery) = do
   let defaultDatabase = "refseq_genomic"                  
   let selectedProgram = fromMaybe defaultProgram program
   let selectedDatabase = fromMaybe defaultDatabase database  
-  result <- performQuery selectedProgram selectedDatabase querySequence entrezQuery counter
-  return result
+  performQuery selectedProgram selectedDatabase querySequence entrezQuery counter
+
 
       
